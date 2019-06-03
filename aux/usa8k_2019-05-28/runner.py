930d5edc10abfb4cb35a415e505f50cc4fdb1e89
Daniel Standage <daniel.standage@nbacc.dhs.gov>
2019-06-03 11:45:20 -0400

    Fix bugs not encountered until runner was executed on HPC

--- aux/usa8k_2019-05-28/runner.py
+++ aux/usa8k_2019-05-28/runner.py
@@ -2,6 +2,14 @@
 from collections import defaultdict
 import pandas
 import sys
+
+
+def pop_str(pop1, pop2):
+    if pop1 == pop2:
+        return pop1
+    return pop1 + ' ' + pop2
+
+
 param_table = pandas.read_csv(sys.stdin, sep='\t')
 params = defaultdict(list)
 for n, values in param_table.iterrows():
@@ -11,16 +19,17 @@ for sampleid, param_lists in sorted(params.items()):
     outfile = sampleid + '.fastq.gz'
     if len(param_lists) == 1:
         gtfile = str(sampleid) + '.genotype.bed'
-        cmd = 'mhpl8r sim --out {of:s} --genotype {gt:s} --panel usa --hapseed {hs:d} --num-reads 250000 --seq-seed {ss:d} --set-threads 1 {pop1:s} {pop2:s}'.format(
+        pops = pop_str(param_lists[0]['MaternalHaploPop'], param_lists[0]['PaternalHaploPop'])
+        cmd = 'mhpl8r sim --out {of:s} --genotype {gt:s} --panel usa --hap-seed {hs:d} --num-reads 250000 --seq-seed {ss:d} --seq-threads 1 {pop:s}'.format(
             of=outfile, gt=gtfile, hs=param_lists[0]['HaploSeed'], ss=param_lists[0]['SeqSeed'],
-            pop1=param_lists[0]['MaternalHaploPop'], pop2=param_lists[0]['PaternalHaploPop']
+            pop=pops
         )
     else:
-        indivstr = ' '.join(['--indiv {} {}'.format(p['MaternalHaploPop'], p['PaternalHaploPop']) for p in param_lists])
+        indivstr = ' '.join(['--indiv ' + pop_str(p['MaternalHaploPop'], p['PaternalHaploPop']) for p in param_lists])
         propstr = ' '.join([str(p['Proportion']) for p in param_lists])
         haplostr = ' '.join([str(p['HaploSeed']) for p in param_lists])
         seqstr = ' '.join([str(p['SeqSeed']) for p in param_lists])
-        cmd = 'mhpl8r mixture {indiv:s} --panel usa --out {of:s} --proporions {prop:s} --hap-seeds {hs:s} --seq-seeds {ss:s} --seq-threads 1'.format(
-           indiv=indivstr, of=outfile, prop=propstr, hs=haplostr, ss=seqstr
+        cmd = 'mhpl8r mixture {indiv:s} --panel usa --out {of:s} --genotype {sid:s}.bed --proportions {prop:s} --hap-seeds {hs:s} --seq-seeds {ss:s} --seq-threads 1'.format(
+           indiv=indivstr, of=outfile, prop=propstr, hs=haplostr, ss=seqstr, sid=sampleid
         )
     print(cmd)
