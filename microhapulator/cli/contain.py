c903b1d7968e96dd311983e3188f34e3c2dab42b
Daniel Standage <daniel.standage@gmail.com>
2019-06-06 23:11:59 -0400

    New `contain` module; `--dry-run` mode for `sim` and `mixture`

---
+++ microhapulator/cli/contain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019, Battelle National Biodefense Institute.
+#
+# This file is part of MicroHapulator (github.com/bioforensics/microhapulator)
+# and is licensed under the BSD license: see LICENSE.txt.
+# -----------------------------------------------------------------------------
+
+
+def subparser(subparsers):
+    cli = subparsers.add_parser('contain')
+    cli.add_argument(
+        '-o', '--out', metavar='FILE', help='write output to "FILE"; by '
+        'default, output is written to the terminal (standard output)'
+    )
+    cli.add_argument(
+        'genotype1', help='simulated or inferred genotype in JSON format'
+    )
+    cli.add_argument(
+        'genotype2', help='simulated or inferred genotype in JSON format'
+    )
---
+++ microhapulator/tests/test_contain.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019, Battelle National Biodefense Institute.
+#
+# This file is part of MicroHapulator (github.com/bioforensics/microhapulator)
+# and is licensed under the BSD license: see LICENSE.txt.
+# -----------------------------------------------------------------------------
+
+import json
+import microhapulator
+from microhapulator.tests import data_file
+import pytest
+from tempfile import NamedTemporaryFile
+
+
+@pytest.mark.parametrize('f1,f2,total,contained', [
+    ('four-brits-sim.json', 'one-brit-sim.json', 38, 38),
+    ('four-brits-sim.json', 'one-american-sim.json', 38, 31),
+    ('one-brit-sim.json', 'one-american-sim.json', 38, 11),
+])
+def test_contain(f1, f2, total, contained):
+    gt1 = microhapulator.genotype.Genotype(data_file(f1))
+    gt2 = microhapulator.genotype.Genotype(data_file(f2))
+    c, t = microhapulator.contain.contain(gt1, gt2)
+    assert t == total
+    assert c == contained
