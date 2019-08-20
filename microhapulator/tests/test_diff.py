bioforensics/MicroHapulator

commit 97cfcb035199dfd07fae5cf5c7aac3dfdb38e6ce

New diff module (#58)
Much like the UNIX diff command shows the differences between two files, this update introduces the `diff` module for showing the differences between two genotypes. Closes #54.

microhapulator/diff.py
@@ -1,0 +1,34 @@
+#!/usr/bin/env python3
+#
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019, Battelle National Biodefense Institute.
+#
+# This file is part of MicroHapulator (github.com/bioforensics/microhapulator)
+# and is licensed under the BSD license: see LICENSE.txt.
+# -----------------------------------------------------------------------------
+
+import json
+import microhapulator
+from microhapulator.genotype import Genotype
+
+
+def diff(gt1, gt2):
+    allloci = set(gt1.loci()).union(gt2.loci())
+    for locus in sorted(allloci):
+        allele1 = gt1.alleles(locus)
+        allele2 = gt2.alleles(locus)
+        diff1 = allele1 - allele2
+        diff2 = allele2 - allele1
+        if len(diff1) > 0 or len(diff2) > 2:
+            yield locus, diff1, diff2
+
+
+def main(args):
+    differ = diff(Genotype(fromfile=args.genotype1), Genotype(fromfile=args.genotype2))
+    with microhapulator.open(args.out, 'w') as fh:
+        for locus, diff1, diff2 in differ:
+            print(locus, file=fh)
+            if len(diff1) > 0:
+                print('>>>', ':'.join(sorted(diff1)), file=fh)
+            if len(diff2) > 0:
+                print('<<<', ':'.join(sorted(diff2)), file=fh)

microhapulator/tests/test_diff.py
@@ -1,0 +1,56 @@
+#!/usr/bin/env python3
+#
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019, Battelle National Biodefense Institute.
+#
+# This file is part of MicroHapulator (github.com/bioforensics/microhapulator)
+# and is licensed under the BSD license: see LICENSE.txt.
+# -----------------------------------------------------------------------------
+
+import json
+import microhapulator
+from microhapulator.tests import data_file
+import pytest
+from tempfile import NamedTemporaryFile
+
+
+def test_diff_basic():
+    gt1 = microhapulator.genotype.SimulatedGenotype(fromfile=data_file('diff-comp-1.json'))
+    gt2 = microhapulator.genotype.SimulatedGenotype(fromfile=data_file('diff-comp-2.json'))
+    diff = list(microhapulator.diff.diff(gt1, gt2))
+    assert diff == [
+        ('MHDBL000140', {'C,C,A,A'}, {'C,C,T,A'}),
+        ('MHDBL000163', {'A,A,G,A,T'}, {'C,G,A,A,T'}),
+    ]
+
+
+def test_diff_large():
+    gt1 = microhapulator.genotype.SimulatedGenotype(fromfile=data_file('diff-comp-1.json'))
+    gt2 = microhapulator.genotype.SimulatedGenotype(fromfile=data_file('diff-comp-3.json'))
+    diff = list(microhapulator.diff.diff(gt1, gt2))
+    loci = [d[0] for d in diff]
+    print(diff[9], diff[13], diff[16])
+    assert loci == [
+        'MHDBL000002', 'MHDBL000007', 'MHDBL000013', 'MHDBL000017', 'MHDBL000018', 'MHDBL000030',
+        'MHDBL000036', 'MHDBL000038', 'MHDBL000058', 'MHDBL000076', 'MHDBL000079', 'MHDBL000082',
+        'MHDBL000085', 'MHDBL000101', 'MHDBL000106', 'MHDBL000111', 'MHDBL000112', 'MHDBL000122',
+        'MHDBL000128', 'MHDBL000129', 'MHDBL000135', 'MHDBL000138', 'MHDBL000140', 'MHDBL000144',
+        'MHDBL000154', 'MHDBL000163', 'MHDBL000183', 'MHDBL000210', 'MHDBL000211', 'MHDBL000212'
+    ]
+    assert diff[9] == ('MHDBL000076', {'G,T'}, set())
+    assert diff[13] == ('MHDBL000101', {'C,C,C,T'}, {'T,C,C,C'})
+    assert diff[16] == ('MHDBL000112', {'G,G,A,C'}, set())
+
+
+def test_diff_cli():
+    f1 = data_file('diff-comp-1.json')
+    f2 = data_file('diff-comp-3.json')
+    with NamedTemporaryFile(suffix='.json') as outfile:
+        arglist = ['diff', '-o', outfile.name, f1, f2]
+        args = microhapulator.cli.get_parser().parse_args(arglist)
+        microhapulator.diff.main(args)
+        with microhapulator.open(outfile.name, 'r') as fh:
+            output = fh.read().strip()
+        with microhapulator.open(data_file('diff-comp-1-3.txt'), 'r') as fh:
+            testoutput = fh.read().strip()
+        assert output == testoutput

microhapulator/cli/diff.py
@@ -1,0 +1,22 @@
+#!/usr/bin/env python3
+#
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019, Battelle National Biodefense Institute.
+#
+# This file is part of MicroHapulator (github.com/bioforensics/microhapulator)
+# and is licensed under the BSD license: see LICENSE.txt.
+# -----------------------------------------------------------------------------
+
+
+def subparser(subparsers):
+    cli = subparsers.add_parser('diff')
+    cli.add_argument(
+        '-o', '--out', metavar='FILE', help='write output to "FILE"; by '
+        'default, output is written to the terminal (standard output)'
+    )
+    cli.add_argument(
+        'genotype1', help='simulated or inferred genotype in JSON format'
+    )
+    cli.add_argument(
+        'genotype2', help='simulated or inferred genotype in JSON format'
+    )

microhapulator/__init__.py
@@ -23,6 +23,7 @@ from microhapulator import panel
 from microhapulator import contain
 from microhapulator import contrib
 from microhapulator import dist
+from microhapulator import diff
 from microhapulator import getrefr
 from microhapulator import mix
 from microhapulator import prob

microhapulator/cli/__init__.py
@@ -13,6 +13,7 @@ import microhapulator
 import sys
 from . import contain
 from . import contrib
+from . import diff
 from . import dist
 from . import getrefr
 from . import mix
@@ -26,6 +27,7 @@ from . import unite
 mains = {
     'contain': microhapulator.contain.main,
     'contrib': microhapulator.contrib.main,
+    'diff': microhapulator.diff.main,
     'dist': microhapulator.dist.main,
     'getrefr': microhapulator.getrefr.main,
     'mix': microhapulator.mix.main,
@@ -40,6 +42,7 @@ mains = {
 subparser_funcs = {
     'contain': contain.subparser,
     'contrib': contrib.subparser,
+    'diff': diff.subparser,
     'dist': dist.subparser,
     'getrefr': getrefr.subparser,
     'mix': mix.subparser,

commit 91b9c4240fc1b1c7ccf0e9136d38854f772b6d30

Fix bug with diff module (#60)
The `diff` module/command introduced in #58 had a typo bug. This update fixes the bug, some of the corresponding smoke tests that missed the bug in the first place, and adds a new regression test to make sure the bug doesn't crop up again.

microhapulator/diff.py
@@ -19,7 +19,7 @@ def diff(gt1, gt2):
         allele2 = gt2.alleles(locus)
         diff1 = allele1 - allele2
         diff2 = allele2 - allele1
-        if len(diff1) > 0 or len(diff2) > 2:
+        if len(diff1) > 0 or len(diff2) > 0:
             yield locus, diff1, diff2
 
 

microhapulator/tests/test_diff.py
@@ -29,17 +29,19 @@ def test_diff_large():
     gt2 = microhapulator.genotype.SimulatedGenotype(fromfile=data_file('diff-comp-3.json'))
     diff = list(microhapulator.diff.diff(gt1, gt2))
     loci = [d[0] for d in diff]
-    print(diff[9], diff[13], diff[16])
+    print(diff[9], diff[17], diff[21])
     assert loci == [
-        'MHDBL000002', 'MHDBL000007', 'MHDBL000013', 'MHDBL000017', 'MHDBL000018', 'MHDBL000030',
-        'MHDBL000036', 'MHDBL000038', 'MHDBL000058', 'MHDBL000076', 'MHDBL000079', 'MHDBL000082',
-        'MHDBL000085', 'MHDBL000101', 'MHDBL000106', 'MHDBL000111', 'MHDBL000112', 'MHDBL000122',
-        'MHDBL000128', 'MHDBL000129', 'MHDBL000135', 'MHDBL000138', 'MHDBL000140', 'MHDBL000144',
-        'MHDBL000154', 'MHDBL000163', 'MHDBL000183', 'MHDBL000210', 'MHDBL000211', 'MHDBL000212'
+        'MHDBL000002', 'MHDBL000003', 'MHDBL000007', 'MHDBL000013', 'MHDBL000017', 'MHDBL000018',
+        'MHDBL000030', 'MHDBL000036', 'MHDBL000038', 'MHDBL000047', 'MHDBL000058', 'MHDBL000061',
+        'MHDBL000076', 'MHDBL000079', 'MHDBL000082', 'MHDBL000085', 'MHDBL000088', 'MHDBL000101',
+        'MHDBL000106', 'MHDBL000108', 'MHDBL000111', 'MHDBL000112', 'MHDBL000122', 'MHDBL000124',
+        'MHDBL000128', 'MHDBL000129', 'MHDBL000135', 'MHDBL000136', 'MHDBL000138', 'MHDBL000140',
+        'MHDBL000144', 'MHDBL000152', 'MHDBL000154', 'MHDBL000163', 'MHDBL000181', 'MHDBL000183',
+        'MHDBL000194', 'MHDBL000210', 'MHDBL000211', 'MHDBL000212'
     ]
-    assert diff[9] == ('MHDBL000076', {'G,T'}, set())
-    assert diff[13] == ('MHDBL000101', {'C,C,C,T'}, {'T,C,C,C'})
-    assert diff[16] == ('MHDBL000112', {'G,G,A,C'}, set())
+    assert diff[9] == ('MHDBL000047', set(), {'T,T'})
+    assert diff[17] == ('MHDBL000101', {'C,C,C,T'}, {'T,C,C,C'})
+    assert diff[21] == ('MHDBL000112', {'G,G,A,C'}, set())
 
 
 def test_diff_cli():
@@ -54,3 +56,10 @@ def test_diff_cli():
         with microhapulator.open(data_file('diff-comp-1-3.txt'), 'r') as fh:
             testoutput = fh.read().strip()
         assert output == testoutput
+
+
+def test_diff2():
+    gt1 = microhapulator.genotype.SimulatedGenotype(fromfile=data_file('euramer-sim-gt.json'))
+    gt2 = microhapulator.genotype.SimulatedGenotype(fromfile=data_file('euramer-inf-gt.json'))
+    diff = list(microhapulator.diff.diff(gt1, gt2))
+    assert diff == [('MHDBL000018', set(), {'T,G,C,T,A'})]