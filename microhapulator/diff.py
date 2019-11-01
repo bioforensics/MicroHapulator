6c8d75fe508e7b2b20b77a639fffd044626811d6
Daniel Standage <daniel.standage@nbacc.dhs.gov>
2019-11-01 11:09:13 -0400

    Update mhpl8r diff module/tests

--- microhapulator/diff.py
+++ microhapulator/diff.py
@@ -12,22 +12,22 @@ import microhapulator
 from microhapulator.profile import Profile
 
 
-def diff(gt1, gt2):
-    allloci = set(gt1.loci()).union(gt2.loci())
-    for locus in sorted(allloci):
-        allele1 = gt1.alleles(locus)
-        allele2 = gt2.alleles(locus)
+def diff(prof1, prof2):
+    allmarkers = set(prof1.markers()).union(prof2.markers())
+    for marker in sorted(allmarkers):
+        allele1 = prof1.alleles(marker)
+        allele2 = prof2.alleles(marker)
         diff1 = allele1 - allele2
         diff2 = allele2 - allele1
         if len(diff1) > 0 or len(diff2) > 0:
-            yield locus, diff1, diff2
+            yield marker, diff1, diff2
 
 
 def main(args):
-    differ = diff(Genotype(fromfile=args.genotype1), Genotype(fromfile=args.genotype2))
+    differ = diff(Profile(fromfile=args.profile1), Profile(fromfile=args.profile2))
     with microhapulator.open(args.out, 'w') as fh:
-        for locus, diff1, diff2 in differ:
-            print(locus, file=fh)
+        for marker, diff1, diff2 in differ:
+            print(marker, file=fh)
             if len(diff1) > 0:
                 print('>>>', ':'.join(sorted(diff1)), file=fh)
             if len(diff2) > 0:
