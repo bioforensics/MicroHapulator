b0c9f54bd3f1a81b485586ed6dfc046849a02e3e
Daniel Standage <daniel.standage@gmail.com>
2021-04-22 15:58:16 -0400

    Add new module/cmd for displaying interlocus balance

---
+++ microhapulator/balance.py
@@ -0,0 +1,42 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021, Battelle National Biodefense Institute.
+#
+# This file is part of MicroHapulator (github.com/bioforensics/microhapulator)
+# and is licensed under the BSD license: see LICENSE.txt.
+# -----------------------------------------------------------------------------
+
+import microhapulator
+import os
+import pandas
+from tempfile import TemporaryDirectory
+import subprocess
+
+
+def count_and_sort(profile, include_discarded=True):
+    counts = dict(
+        Marker=list(),
+        ReadCount=list(),
+    )
+    for marker, mdata in profile.data["markers"].items():
+        readcount = 0
+        if include_discarded:
+            readcount += mdata['num_discarded_reads']
+        for haplotype, count in mdata['allele_counts'].items():
+            readcount += count
+        counts['Marker'].append(marker)
+        counts['ReadCount'].append(readcount)
+    data = pandas.DataFrame(counts).sort_values(['ReadCount'], ascending=False)
+    return data
+
+
+def balance(profile, include_discarded=True):
+    data = count_and_sort(profile, include_discarded=include_discarded)
+    with TemporaryDirectory() as tempdir:
+        tfile = os.path.join(tempdir, "data.tsv")
+        data.to_csv(tfile, index=False, header=False)
+        subprocess.run(['termgraph', tfile])
+
+
+def main(args):
+    profile = microhapulator.profile.ObservedProfile(fromfile=args.input)
+    balance(profile, include_discarded=args.discarded)
---
+++ microhapulator/cli/balance.py
@@ -0,0 +1,19 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021, Battelle National Biodefense Institute.
+#
+# This file is part of MicroHapulator (github.com/bioforensics/microhapulator)
+# and is licensed under the BSD license: see LICENSE.txt.
+# -----------------------------------------------------------------------------
+
+import sys
+
+
+def subparser(subparsers):
+    cli = subparsers.add_parser('balance')
+    cli.add_argument(
+        '-D', '--no-discarded', dest='discarded', action='store_false',
+        help='do not included mapping but discarded reads in read counts; by default, reads that '
+        'are mapped to the marker but discarded because they do not span all variants at the '
+        'marker are included'
+    )
+    cli.add_argument('input', help='typing result in JSON format')
