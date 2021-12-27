ea8ff1931d16b710d2856243b60302d0d5bb4fa5
Daniel Standage <daniel.standage@nbacc.dhs.gov>
2021-12-27 15:17:35 -0500

    Update Python headers (#96)

--- microhapulator/tests/test_loading_functions.py
+++ microhapulator/tests/test_loading_functions.py
@@ -1,9 +1,14 @@
-# -----------------------------------------------------------------------------
-# Copyright (c) 2021, Battelle National Biodefense Institute.
+# -------------------------------------------------------------------------------------------------
+# Copyright (c) 2021, DHS.
 #
-# This file is part of MicroHapulator (github.com/bioforensics/microhapulator)
-# and is licensed under the BSD license: see LICENSE.txt.
-# -----------------------------------------------------------------------------
+# This file is part of MicroHapulator (https://github.com/bioforensics/microhapulator) and is
+# licensed under the BSD license: see LICENSE.txt.
+#
+# This software was prepared for the Department of Homeland Security (DHS) by the Battelle National
+# Biodefense Institute, LLC (BNBI) as part of contract HSHQDC-15-C-00064 to manage and operate the
+# National Biodefense Analysis and Countermeasures Center (NBACC), a Federally Funded Research and
+# Development Center.
+# -------------------------------------------------------------------------------------------------
 
 import microhapulator
 from microhapulator.tests import data_file
