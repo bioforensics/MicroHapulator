a9f6b33d69aa47f0aca8d69424b69f84ed32a4fc
Daniel Standage <daniel.standage@st.dhs.gov>
2022-01-14 14:35:38 -0500

    Update test suite

--- microhapulator/tests/test_filter.py
+++ microhapulator/tests/test_filter.py
@@ -14,6 +14,7 @@ import microhapulator
 import microhapulator.api as mhapi
 from microhapulator.profile import TypingResult
 from microhapulator.tests import data_file
+import pandas as pd
 import pytest
 
 
@@ -24,6 +25,30 @@ def test_filter_simple():
     observed.filter(static=10, dynamic=0.05)
     expected = TypingResult(fromfile=data_file("pashtun-sim/test-output.json"))
     assert observed == expected
+
+
+def test_filter_config_file():
+    config = pd.read_csv(data_file("filters.csv"), sep=None, engine="python")
+    result = TypingResult(fromfile=data_file("prof/deep-raw.json"))
+    result.filter(static=5, dynamic=0.02, config=config)
+    assert len(result.haplotypes("mh01XYZ-1")) == 8
+    assert len(result.haplotypes("mh02XYZ-2")) == 2
+    assert len(result.haplotypes("mh02XYZ-3")) == 2
+
+
+def test_filter_missing_column():
+    config = pd.read_csv(data_file("filters-missing.csv"), sep=None, engine="python")
+    result = TypingResult(fromfile=data_file("prof/deep-raw.json"))
+    with pytest.raises(ValueError, match=r"filter config file missing column\(s\): Static"):
+        result.filter(static=5, dynamic=0.02, config=config)
+
+
+def test_filter_dupl_marker():
+    config = pd.read_csv(data_file("filters-redundant.csv"), sep=None, engine="python")
+    result = TypingResult(fromfile=data_file("prof/deep-raw.json"))
+    message = "filter config file contains duplicate entries for some markers"
+    with pytest.raises(ValueError, match=message):
+        result.filter(static=5, dynamic=0.02, config=config)
 
 
 def test_filter_cli(tmp_path):
@@ -43,4 +68,26 @@ def test_filter_cli(tmp_path):
     microhapulator.cli.filter.main(args)
     observed = TypingResult(fromfile=filtered)
     expected = TypingResult(fromfile=data_file("pashtun-sim/test-output.json"))
+    assert observed == expected
+
+
+def test_filter_cli_config(tmp_path):
+    unfiltered = data_file("prof/deep-raw.json")
+    filtered = str(tmp_path / "genotype-call.json")
+    arglist = [
+        "filter",
+        unfiltered,
+        "--out",
+        filtered,
+        "--static",
+        "5",
+        "--dynamic",
+        "0.02",
+        "--config",
+        data_file("filters.csv"),
+    ]
+    args = microhapulator.cli.get_parser().parse_args(arglist)
+    microhapulator.cli.filter.main(args)
+    observed = TypingResult(fromfile=filtered)
+    expected = TypingResult(fromfile=data_file("prof/deep-filt.json"))
     assert observed == expected
