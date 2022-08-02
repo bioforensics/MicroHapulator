ae08e8c05b91e7d06e595519f477aaf87ca7328a
Daniel Standage <daniel.standage@gmail.com>
2022-08-02 13:31:01 -0400

    Bug fixes [skip ci]

--- microhapulator/pipeaux.py
+++ microhapulator/pipeaux.py
@@ -62,14 +62,16 @@ def parse_balance_stat(logfile):
 def encode(filepath):
     with open(filepath, "rb") as fh:
         return b64encode(fh.read()).decode("ascii")
-    
+
 
 def per_marker_typing_rate(samples):
     sample_rates = dict()
     for sample in samples:
-        filename = Path("analysis") / sample / f"{sample}-typing-rate.tsv"
-        sample_rates[sample] = pd.read_csv(filename, sep="\t")
-    all_rates = pd.concat([df.TypingRate for df in sample_rates.values()], axis=1, keys=sample_rates.keys())
+        filename = f"analysis/{sample}/{sample}-typing-rate.tsv"
+        sample_rates[sample] = pd.read_csv(filename, sep="\t").set_index("Marker")
+    all_rates = pd.concat(
+        [df.TypingRate for df in sample_rates.values()], axis=1, keys=sample_rates.keys()
+    )
     return all_rates
 
 
@@ -89,7 +91,7 @@ def final_html_report(samples, summary):
     if read_length_table is not None:
         col = ("Sample", "LengthR1", "LengthR2")
         read_length_table = pd.DataFrame(read_length_table, columns=col)
-    typing_rate = per_marker_typing_rate(samples)
+    typing_rates = per_marker_typing_rate(samples)
     plots = {
         "r1readlen": list(),
         "r2readlen": list(),
