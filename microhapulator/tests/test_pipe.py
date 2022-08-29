7a3c1e922440f86afab792f5581929d77f7265d4
agshumate <alainashumate@gmail.com>
2022-08-29 15:21:01 -0400

    changed reference genome download to happen only if the index is not present

--- microhapulator/tests/test_pipe.py
+++ microhapulator/tests/test_pipe.py
@@ -39,8 +39,6 @@ def test_pipe_missing_files(tmp_path):
 
 
 def test_pipe_gbr_usc10(tmp_path):
-    hg38_placeholder = str("microhapulator/data/hg38.fasta")
-    copyfile(data_file("refr/usc10-refr.fna"), hg38_placeholder)
     arglist = [
         "pipe",
         data_file("refr/usc10-refr.fna"),
@@ -52,8 +50,6 @@ def test_pipe_gbr_usc10(tmp_path):
         "--threads=1",
         "--static=5",
         "--dynamic=0.02",
-        "--hg38",
-        hg38_placeholder,
     ]
     args = microhapulator.cli.get_parser().parse_args(arglist)
     microhapulator.cli.pipe.main(args)
