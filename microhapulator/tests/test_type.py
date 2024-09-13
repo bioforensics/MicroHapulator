84444a372ebed4e218f18bb12e0a5200721232ad
Daniel Standage <daniel.standage@gmail.com>
2024-09-13 13:04:06 -0400

    Mark gaps instead of discarding (#185)

--- microhapulator/tests/test_type.py
+++ microhapulator/tests/test_type.py
@@ -25,21 +25,22 @@ def test_type_simple():
     result = mhapi.type(bam, tsv)
     assert result.haplotypes("mh13KK-218") == set()
     assert result.data["markers"]["mh13KK-218"]["typing_result"] == {
-        "C,T,C,G": 1,
-        "C,T,T,T": 1,
+        "T,T,C,T": 1178,
+        "T,T,T,T": 1170,
+        "T,T,T,G": 6,
+        "T,T,C,G": 2,
         "G,T,C,T": 1,
-        "T,A,C,T": 1,
-        "T,G,C,T": 3,
+        "T,T,C,A": 1,
         "T,G,T,T": 2,
         "T,T,A,T": 5,
-        "T,T,C,A": 1,
-        "T,T,C,C": 2,
-        "T,T,C,G": 2,
-        "T,T,C,T": 1178,
+        "T,G,C,T": 3,
+        "T,A,C,T": 1,
+        "C,T,C,G": 1,
         "T,T,G,T": 2,
+        "T,T,C,C": 2,
         "T,T,T,A": 2,
-        "T,T,T,G": 6,
-        "T,T,T,T": 1170,
+        "T,T,C,-": 1,
+        "C,T,T,T": 1,
     }
     assert result.haplotypes("mh21KK-320") == set()
     assert result.data["markers"]["mh21KK-320"]["typing_result"] == {
@@ -53,6 +54,7 @@ def test_type_simple():
         "G,C,C,A": 1,
         "G,C,T,A": 4,
         "G,G,A,A": 2,
+        "G,A,G,-": 1,
         "G,G,A,T": 1,
         "G,G,C,A": 1075,
         "G,G,C,C": 3,
@@ -119,6 +121,7 @@ def test_type_cli_simple(tmp_path):
         "T,T,C,C": 2,
         "T,T,C,G": 2,
         "T,T,C,T": 1178,
+        "T,T,C,-": 1,
         "T,T,G,T": 2,
         "T,T,T,A": 2,
         "T,T,T,G": 6,
@@ -129,6 +132,7 @@ def test_type_cli_simple(tmp_path):
         "G,A,A,A": 1,
         "G,A,C,A": 3,
         "G,A,G,A": 3,
+        "G,A,G,-": 1,
         "G,A,T,A": 1075,
         "G,A,T,C": 1,
         "G,A,T,G": 1,
@@ -177,3 +181,14 @@ def test_type_marker_with_no_coverage():
     result = mhapi.type(bam, tsv)
     for marker, mdata in result.data["markers"].items():
         assert "genotype" in mdata
+
+
+def test_type_marker_has_indel_spanning_snp():
+    bam = data_file("bam/indel_snp.bam")
+    tsv = data_file("def/indel_snp.tsv")
+    result = mhapi.type(bam, tsv)
+    assert "mh09WL-026" in result.data["markers"]
+    for allele in result.data["markers"]["mh09WL-026"]["typing_result"]:
+        snp_alleles = allele.split(",")
+        assert len(snp_alleles) == 6
+        assert snp_alleles[-2] == "-"
